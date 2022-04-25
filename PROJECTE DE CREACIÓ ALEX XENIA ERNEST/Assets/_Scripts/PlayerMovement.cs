f8eed59bc9d3ab2f987ce18d6236a43511adbc0c
Alexandro Vera <averap@edu.tecnocampus.cat>
2022-04-25 21:38:49 +0200

    Movement (start) + added tiles

---
+++ PROJECTE DE CREACIÓ ALEX XENIA ERNEST/Assets/_Scripts/PlayerMovement.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed = 1f;
+    public float horizontal;
+    public float vertical;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        float y = horizontal * speed * Time.deltaTime;
+        float x = vertical * speed * Time.deltaTime;
+
+        transform.Translate(new Vector3(x,y,0));
+    }
+    public void OnMove(InputValue inputValue)
+    {
+        var move2d = inputValue.Get<Vector2>();
+        horizontal = move2d.y;
+        vertical = move2d.x;
+
+
+        Debug.Log("OnMove");
+    }
+}
