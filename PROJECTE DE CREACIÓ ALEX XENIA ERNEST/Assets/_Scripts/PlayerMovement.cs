86d9882e4d5b39eac83a81ae5c161030d56a9f86
Alexandro Vera <averap@edu.tecnocampus.cat>
2022-04-29 11:18:19 +0200

    CameraShake

--- PROJECTE DE CREACIÓ ALEX XENIA ERNEST/Assets/_Scripts/PlayerMovement.cs
+++ PROJECTE DE CREACIÓ ALEX XENIA ERNEST/Assets/_Scripts/PlayerMovement.cs
@@ -32,10 +32,14 @@ public class PlayerMovement : MonoBehaviour
     public GameObject tree;
     public float disappearTime = 1f;
 
+    Shake_Camera camerashake;
+
     void Start()
     {
         activeSpeed = speed;
         rb = this.GetComponent<Rigidbody2D>();
+        camerashake = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Shake_Camera>();
+
     }
 
     // Update is called once per frame
@@ -89,6 +93,7 @@ public class PlayerMovement : MonoBehaviour
         {
             Instantiate(dashEffect, transform.position, Quaternion.identity);
             //Instantiate(tree, transform.position, Quaternion.identity);
+            camerashake.Shake();
 
 
 
