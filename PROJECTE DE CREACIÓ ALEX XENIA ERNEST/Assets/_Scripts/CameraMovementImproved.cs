27ee5533d866834b536607a4470ed8b5c2c90ba1
ErnestIb <eibanez@edu.tecnocampus.cat>
2022-04-30 01:11:16 +0200

    GRAN UPDATE CAMARA
    CAMARA AMB EL INPUT SYSTEM NOU
    ARA ES MOU QUAN S'APRETA EL SHIFT
    EN PROCÉS D'IMPLEMENTECIÓ DE FUNCIÓ ZOOM

---
+++ PROJECTE DE CREACIÓ ALEX XENIA ERNEST/Assets/_Scripts/CameraMovementImproved.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovementImproved : MonoBehaviour
+{
+    [SerializeField]
+    private Camera cam;
+    private Vector3 dragOrigin;
+
+    void Update()
+    {
+        PanCamera();
+    }
+
+    public void PanCamera()
+    {
+        Debug.Log("move camera!");
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            dragOrigin = cam.ScreenToWorldPoint(Input.mousePosition);
+
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 difference = dragOrigin - cam.ScreenToWorldPoint(Input.mousePosition);
+
+            print("origin " + dragOrigin + " new position " + cam.ScreenToWorldPoint(Input.mousePosition) + " =difference" + difference);
+
+            cam.transform.position += difference;
+        }
+
+    }
+}
