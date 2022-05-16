b8637728fa2a947d5231ec71ec4a40533671d45a
XeniaRobert <xrobert@edu.tecnocampus.cat>
2022-05-16 15:21:45 +0200

    srasdv

--- PROJECTE DE CREACIÓ ALEX XENIA ERNEST/Assets/_Scripts/SlimeEnemic.cs
+++ PROJECTE DE CREACIÓ ALEX XENIA ERNEST/Assets/_Scripts/SlimeEnemic.cs
@@ -4,16 +4,21 @@ using UnityEngine;
 
 public class SlimeEnemic : MonoBehaviour, ITakeDamage
 {
+    private SpriteRenderer slime;
+
     public float life = 50;
 
     [SerializeField] List<Transform> wayPoints;
     public float velocity = 2;
+    public float damage = 10;
     float distanciaCambio = 0.2f;
     byte siguientePos = 0;
     // Start is called before the first frame update
     void Start()
     {
+        slime = GetComponent<SpriteRenderer>();
         
+        transform.position = wayPoints[0].transform.position;
     }
 
     // Update is called once per frame
@@ -35,9 +40,21 @@ public class SlimeEnemic : MonoBehaviour, ITakeDamage
     {
         life -= damage;
 
+        Color newColor = new Color(255f / 255f, 100f / 255f, 100f / 255f);
+        slime.color = newColor;
+
         if (life <= 0)
         {
             Destroy(this.gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        var damageTaker = other.GetComponent<ITakeDamage>();
+        if (other.CompareTag("Player"))
+        {
+            damageTaker.TakeDamage(damage);
         }
     }
 }
