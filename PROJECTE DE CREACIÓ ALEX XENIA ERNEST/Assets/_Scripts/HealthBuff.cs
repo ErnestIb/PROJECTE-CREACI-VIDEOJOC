9336f523419e4c5fa0c7cd3f489c3cd65d562f55
Ernest <eibanez@edu.tecnocampus.cat>
2022-05-16 17:01:44 +0200

    ANEM ENRERE

---
+++ PROJECTE DE CREACIÓ ALEX XENIA ERNEST/Assets/_Scripts/HealthBuff.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Powerups/HealthBuff")]
+
+public class HealthBuff : PowerUpEffect
+{
+    public float amount;
+
+    public override void Apply(GameObject target)
+    {
+
+    }
+}
---
+++ PROJECTE DE CREACIÓ ALEX XENIA ERNEST/Assets/_Scripts/SpeedBuff.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Threading.Timer;
+
+[CreateAssetMenu(menuName = "Powerups/SpeedBuff")]
+public class SpeedBuff : PowerUpEffect
+{
+    public float amount;
+    public float buffDuration = 5f;
+
+    public override void Apply(GameObject target)
+    {
+        target.GetComponent<PlayerMovement>().activeSpeed += amount;
+
+        aTimer = new System.Timers.Timer(1000 * 60 * 3);
+        aTimer.Elapsed += new ElapsedEventHandler(OnTimedEvent);
+        aTimer.Enabled = true;
+        target.GetComponent<PlayerMovement>().activeSpeed -= amount;
+
+    }
+
+    public void DeBuff(GameObject target)
+    {
+        target.GetComponent<PlayerMovement>().activeSpeed -= amount;
+    }
+
+    
+
+}
