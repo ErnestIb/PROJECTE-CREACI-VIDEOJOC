fa6c0d9e89602b0a1dcad582e1f367b8bcb03b6e
XeniaRobert <xrobert@edu.tecnocampus.cat>
2022-05-16 12:22:24 +0200

    ns

---
+++ PROJECTE DE CREACIÓ ALEX XENIA ERNEST/Assets/_Scripts/SlimeEnemic.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlimeEnemic : MonoBehaviour, ITakeDamage
+{
+    public float life = 50;
+
+    [SerializeField] List<Transform> wayPoints;
+    public float velocity = 2;
+    float distanciaCambio = 0.2f;
+    byte siguientePos = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, wayPoints[siguientePos].transform.position, velocity * Time.deltaTime);
+
+        if (Vector3.Distance(transform.position, wayPoints[siguientePos].transform.position) < distanciaCambio)
+        {
+            siguientePos++;
+            if(siguientePos >= wayPoints.Count)
+            {
+                siguientePos = 0;
+            }
+        }
+    }
+    
+    public void TakeDamage(float damage)
+    {
+        life -= damage;
+
+        if (life <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
