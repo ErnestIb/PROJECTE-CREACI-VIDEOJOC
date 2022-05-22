769414a9390962e80d716bee59e6291fe2609302
ErnestIb <eibanez@edu.tecnocampus.cat>
2022-05-22 13:20:29 +0200

    ManaBar

---
+++ PROJECTE DE CREACIÓ ALEX XENIA ERNEST/Assets/_Scripts/ManaBar.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ManaBar : MonoBehaviour
+{
+    public Slider slider;
+    public Gradient gradient;
+    public Image fill;
+
+    public void SetMaxShield(int mana)
+    {
+        slider.maxValue = mana;
+        slider.value = 0;
+
+        fill.color = gradient.Evaluate(1f);
+    }
+
+    public void SetShield(int mana)
+    {
+        slider.value += mana;
+
+        fill.color = gradient.Evaluate(slider.normalizedValue);
+    }
+}
