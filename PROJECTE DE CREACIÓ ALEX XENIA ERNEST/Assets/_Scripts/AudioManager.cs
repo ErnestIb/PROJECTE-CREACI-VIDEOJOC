851208d5c098dd33ecee7d8f687a8c049540d243
XeniaRobert <xrobert@edu.tecnocampus.cat>
2022-05-31 19:35:10 +0200

    asdas

---
+++ PROJECTE DE CREACIÓ ALEX XENIA ERNEST/Assets/_Scripts/AudioManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+//public class AudioManager : MonoBehaviour
+//{
+
+//    public static AudioManager Instance;  //singleton
+
+//    [SerializeField] private AudioSource _audioSource;
+
+
+    
+//    void Awake()
+//    {
+
+//        if (Instance)
+//        {
+//            Instance = this;
+//        }
+//        else
+//        {
+//            Destroy(gameObject);
+//        }
+
+//    }
+
+//    private void Start()
+//    {
+//        _audioSource = gameObject.GetComponent<AudioSource>();
+//    }
+
+//    public static void PlaySound(string v)
+//    {
+//        Instance._PlaySound(v);
+//    }
+
+//    private void _PlaySound(string v)
+//    {
+        
+//    }
+
+//    // Update is called once per frame
+//    void Update()
+//    {
+//        AudioManager.PlaySound("s");
+//    }
+//}
