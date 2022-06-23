8b33c20c247b99fed06ccc9685220e6cdab8abe4
Alexandro Vera <averap@edu.tecnocampus.cat>
2022-06-23 15:13:37 +0200

    a

---
+++ PROJECTE DE CREACIÓ ALEX XENIA ERNEST/Assets/_Scripts/AlexScripts/PauseMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public void OnPause()
+    {
+        Time.timeScale = 0;
+        gameObject.SetActive(true);
+    }    
+
+    public void Resume()
+    {
+        Time.timeScale = 1;
+        gameObject.SetActive(false);
+    }
+}
